410dd0461a5bf3dbcfb0e56ab716fc55b2a6c8aa
srayabar@Office <naath.r@gmail.com>
2018-06-08 18:18:16 +0530

    few code changes to understand SpringBoot

--- SpringBootTest/src/main/java/com/springboot/repositories/ITopicRepository.java
+++ SpringBootTest/src/main/java/com/springboot/repositories/ITopicRepository.java
@@ -4,6 +4,10 @@ import org.springframework.data.repository.CrudRepository;
 
 import com.springboot.pojos.Topic;
 
+/**
+ * CrudRepository is a common repository which contains all general methods for CRUD operations
+ *
+ */
 public interface ITopicRepository extends CrudRepository<Topic, String>	{ //Topic and it's Id column
 	
 }
