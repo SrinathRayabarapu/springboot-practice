d4881b09d95c50b3354b39a073769bd9c05b5069
srayabar@Office <naath.r@gmail.com>
2018-06-13 22:18:53 +0530

    DI - literal injection bean scope - Singleton example

--- SpringBootTest/src/main/java/com/springboot/SpringCoreApp.java
+++ SpringBootTest/src/main/java/com/springboot/SpringCoreApp.java
@@ -3,6 +3,7 @@ package com.springboot;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 import com.springboot.bean.Coach;
+import com.springboot.bean.CricketCoach;
 
 /**
  * class designed to explore spring core functionalities
@@ -15,15 +16,38 @@ public class SpringCoreApp {
 	public static void main(String[] args) {
 		
 		ClassPathXmlApplicationContext applicationContext = new ClassPathXmlApplicationContext("applicationContext.xml");
-		Coach bean = applicationContext.getBean("trackCoach", Coach.class); //creating and managing Object creation - Inversion of Control
 		
+		//constructor DI
+		Coach bean = applicationContext.getBean("trackCoach", Coach.class); //creating and managing Object creation - Inversion of Control		
 		//this is Inversion of Control(IOC) where Spring only manages Object 
-		bean.doWorkOut();
-		
+		bean.doWorkOut();		
 		// this is where DI comes in to picture where it injects an Object to another class constructor
 		bean.getFortune();
 		
+		//setter method DI
+		CricketCoach cricketCoach = applicationContext.getBean("cricketCoach", CricketCoach.class);
+		
+		cricketCoach.doWorkOut();
+		
+		cricketCoach.getFortune();
+		
+		//literal dependency injections
+		cricketCoach.getEmailAddress();
+		cricketCoach.getTeamName();
+		
+		cricketCoach.setUserValue("value set by Shubha!");
 		
 		
+		CricketCoach cricketCoach1 = applicationContext.getBean("cricketCoach", CricketCoach.class);
+		//default scope of bean is Singleton - value set by Shubha! will be printed
+		System.out.println(cricketCoach1.getUserValue());
+		
+		
+		
+		
+		
+		
+		//closing the context
+		applicationContext.close();
 	}
 }
--- SpringBootTest/src/main/java/com/springboot/bean/TrackCoach.java
+++ SpringBootTest/src/main/java/com/springboot/bean/TrackCoach.java
@@ -6,6 +6,8 @@ public class TrackCoach implements Coach{
 	
 	private FortuneService fortuneService;
 	
+	private String userValue;
+	
 	//this is where dependency injection happens
 	public TrackCoach(FortuneService fortuneService) {
 		this.fortuneService = fortuneService;
@@ -19,6 +21,20 @@ public class TrackCoach implements Coach{
 	@Override
 	public void getFortune() {
 		fortuneService.getFortune();
+	}
+
+	/**
+	 * @return the userValue
+	 */
+	public String getUserValue() {
+		return userValue;
+	}
+
+	/**
+	 * @param userValue the userValue to set
+	 */
+	public void setUserValue(String userValue) {
+		this.userValue = userValue;
 	}
 
 }
