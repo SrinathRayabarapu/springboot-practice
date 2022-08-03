7c6ede2608825e63863fdb4720e50d14b1fbda96
Srinath Rayabarapu <srinath.rayabarapu@tesco.com>
2022-08-03 15:19:28 +0530

    Restructured the project

--- springboot-general/src/main/java/com/boot/SpringBootPracticeApp.java
+++ springboot-general/src/main/java/com/boot/SpringBootPracticeApp.java
@@ -4,7 +4,6 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-
 /**
  * Main class in spring boot. It initializes all the components, entities etc in spring.boot package.
  * 
