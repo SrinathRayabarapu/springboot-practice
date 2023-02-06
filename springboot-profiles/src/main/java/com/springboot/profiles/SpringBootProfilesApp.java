5ffb7946d0ab7bf11d4ca517555d0a9b4b8ed0ee
Srinath Rayabarapu <naath.r@gmail.com>
2023-02-06 13:23:08 +0530

    Comments updated

--- springboot-profiles/src/main/java/com/springboot/profiles/SpringBootProfilesApp.java
+++ springboot-profiles/src/main/java/com/springboot/profiles/SpringBootProfilesApp.java
@@ -4,6 +4,11 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * Spring profiles are implemented for different environments. <p>
+ *
+ * Use the jvm parameter : -Dspring.profiles.active=dev
+ */
 @Slf4j
 @SpringBootApplication
 public class SpringBootProfilesApp {
