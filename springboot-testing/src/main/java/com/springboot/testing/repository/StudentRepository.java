61def242c504b4a484623bae84f7ac2ed3d668ae
Srinath Rayabarapu <naath.r@gmail.com>
2024-05-01 22:27:56 +0530

    SpringBoot unit testing

--- springboot-testing/src/main/java/com/springboot/testing/repository/StudentRepository.java
+++ springboot-testing/src/main/java/com/springboot/testing/repository/StudentRepository.java
@@ -4,5 +4,7 @@ import com.springboot.testing.entities.Student;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface StudentRepository extends JpaRepository<Student, Long> {
+
     Student getStudentByName(String name);
+
 }
